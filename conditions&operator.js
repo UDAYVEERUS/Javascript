5a61ee7a47f721055e3d549dcfc884279c78e4a8
UDAYVEERUS <udayveerus348567@gmail.com>
2023-11-30 11:55:47 +0530

    condition

--- conditions&operator.js
+++ conditions&operator.js
@@ -1,11 +1,21 @@
 // Arithmatic operations
-let a = 7;
-let b = 2;
+// let a = 7;
+// let b = 2;
 
-console.log("a = " , a , " b = ", b);
-console.log("a + b = ", a + b); // 9
-console.log("a - b = ", a - b); // 5
-console.log("a * b = ", a * b); // 14
-console.log("a / b = ", a / b); // 3.5
-console.log("a % b = ", a % b); // 1
-console.log("a == b" , a == b); // false
+// console.log("a = " , a , " b = ", b);
+// console.log("a + b = ", a + b); // 9
+// console.log("a - b = ", a - b); // 5
+// console.log("a * b = ", a * b); // 14
+// console.log("a / b = ", a / b); // 3.5
+// console.log("a % b = ", a % b); // 1
+// console.log("a == b" , a == b); // false
+
+
+
+let age = 16;
+
+if(age > 18){
+    console.log("you can vote");
+}else{
+    console.log("You can not vote.")
+}
