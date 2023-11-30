5566b7fae81cbe092164963c235cfca47631e19e
UDAYVEERUS <udayveerus348567@gmail.com>
2023-11-30 12:35:57 +0530

    Star pattern print

--- conditions&operator.js
+++ conditions&operator.js
@@ -25,21 +25,26 @@
 // let res2 = (age > 20 || age < 13);
 // console.log(res2); // false
 
-let score = 67;
+// let score = 67;
 
-if(score >= 90 && score <= 100){
-    grade = "A"
-}else if(score >= 80 && score <= 89){
-    grade = "B"
-}else if(score >= 70 && score <= 79){
-    grade = "C"
-} else if(score >= 60 && score <= 69){
-    grade = "D"
-}else if(score >=50 && score <= 59){
-    grade = "E"
-}
-else{
-    grade = "F"
-}
-console.log(grade);
+// if(score >= 90 && score <= 100){
+//     grade = "A"
+// }else if(score >= 80 && score <= 89){
+//     grade = "B"
+// }else if(score >= 70 && score <= 79){
+//     grade = "C"
+// } else if(score >= 60 && score <= 69){
+//     grade = "D"
+// }else if(score >=50 && score <= 59){
+//     grade = "E"
+// }
+// else{
+//     grade = "F"
+// }
+// console.log(grade);
 
+// For loop * pattern
+
+for(let i = 5; i >= 1 ; i--) {
+    console.log(" * ".repeat(i) )
+}
