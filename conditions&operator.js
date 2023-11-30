44004c1e751fb753d5499e4f9bf7a60a73101e64
UDAYVEERUS <udayveerus348567@gmail.com>
2023-11-30 11:50:27 +0530

    logocal operator

---
+++ conditions&operator.js
@@ -0,0 +1,11 @@
+// Arithmatic operations
+let a = 7;
+let b = 2;
+
+console.log("a = " , a , " b = ", b);
+console.log("a + b = ", a + b); // 9
+console.log("a - b = ", a - b); // 5
+console.log("a * b = ", a * b); // 14
+console.log("a / b = ", a / b); // 3.5
+console.log("a % b = ", a % b); // 1
+console.log("a == b" , a == b); // false
