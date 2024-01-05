ed7e2ed0121d645655856af64be64e2728f8164e
UDAYVEERUS <udayveerus348567@gmail.com>
2024-01-05 10:17:59 +0530

    write a normal function

--- curring.js
+++ curring.js
@@ -1,9 +1,16 @@
+// normal function
+function Volume1(length, width, height) {
+    return length * width * height;
+}
+let multiply = Volume1(10, 20, 30);
+console.log(multiply);
 
-// currying function
-function Volume(length){
-    return function (width){
-        return function (height){
-            return length*width*height;
+
+//let's convert it into currying function currying function
+function Volume(length) {
+    return function (width) {
+        return function (height) {
+            return length * width * height;
         }
     }
 }
@@ -14,16 +21,16 @@ let fn3 = fn2(30);
 console.log(fn3);
 
 // Normal function
-function findSum(val1, val2){
+function findSum(val1, val2) {
     return val1 + val2;
 }
-console.log(findSum(2,4));
+console.log(findSum(2, 4));
 
 
 // let's convert it into currying function
 
-function findSum1(val1){
-    return function(val2){
+function findSum1(val1) {
+    return function (val2) {
         return val1 - val2;
     }
 }
